0508b43a99d912ea81028dcee6b7284b5b915a59
Jatin Verma <jatin95oct@gmail.com>
2018-03-26 12:35:51 +0530

    stackEvaluationExp

--- Stackk/Client.java
+++ Stackk/Client.java
@@ -2,9 +2,9 @@ package Stackk;
 
 public class Client {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 
-		Stackkk st=new Stackkk();
+		Stackkk<Integer> st = new Stackkk<>();
 		st.push(10);
 		st.push(20);
 		st.push(30);
@@ -13,9 +13,11 @@ public class Client {
 		st.pop();
 		st.push(50);
 		st.display();
-		
-		
-		
+
+		st.infixToPostfix("a+b*(c^d-e)^(f+g*h)-i");
+
+		st.evaluatePostfix("231*+9-");
+
 	}
 
 }
--- Stackk/Stackkk.java
+++ Stackk/Stackkk.java
@@ -1,7 +1,7 @@
 package Stackk;
 
-public class Stackkk {
-	protected int[] data;
+public class Stackkk<T> {
+	protected T[] data;
 	protected int tos;
 
 	public Stackkk() {
@@ -9,12 +9,12 @@ public class Stackkk {
 	}
 
 	public Stackkk(int cap) {
-		data = new int[cap];
+		data = (T[]) new Object[cap];
 		tos = -1;
 	}
 
-	public void push(int item) throws Exception {
-		if (data.length == tos - 1) {
+	public void push(T item) throws Exception {
+		if (data.length == tos) {
 			throw new Exception("Stack full");
 		}
 
@@ -23,27 +23,27 @@ public class Stackkk {
 
 	}
 
-	public int pop() throws Exception {
+	public T pop() throws Exception {
 		if (this.tos == -1) {
 			throw new Exception("Stack empty");
 		}
-		int rv = this.data[this.tos];
+		T rv = this.data[this.tos];
 		this.tos--;
 		return rv;
 
 	}
 
-	public int top() throws Exception {
+	public T top() throws Exception {
 		if (this.tos == -1) {
 			throw new Exception("Stack empty");
 		}
-		int rv = this.data[this.tos];
+		T rv = this.data[this.tos];
 		return rv;
 
 	}
 
 	public int size() {
-		return ++this.tos;
+		return this.tos + 1;
 	}
 
 	public boolean isEmpty() {
@@ -59,6 +59,120 @@ public class Stackkk {
 			System.out.println(this.data[i]);
 		}
 		System.out.println("END");
+
+	}
+
+	public void infixToPostfix(String exp) throws Exception {
+		Stackkk<Character> tempStack = new Stackkk<>();
+		String res = "";
+		while (exp.length() != 0) {
+			char ch = exp.charAt(0);
+			exp = exp.substring(1);
+
+			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+				res += ch;
+			} else if (ch == '(') {
+				tempStack.push(ch);
+			} else if ("+-*/^)".indexOf(ch) != -1) {
+				if (ch == ')') {
+					while (!tempStack.isEmpty() && tempStack.top() != '(') {
+						res += tempStack.pop();
+					}
+					tempStack.pop();
+
+				} else {
+
+					if (!tempStack.isEmpty()) {
+						int p = priorityFind(ch);
+						int tosp = priorityFind(tempStack.top());
+						if (p < tosp) {
+							while (!tempStack.isEmpty() && p <= priorityFind(tempStack.top())) {
+								res += tempStack.pop();
+							}
+
+						}
+					}
+					tempStack.push(ch);
+				}
+			}
+		}
+
+		while (tempStack.size() > 0) {
+			res += tempStack.pop();
+		}
+
+		for (int i = 0; i < res.length(); i++) {
+			System.out.print(res.charAt(i));
+		}
+		System.out.println();
+
+	}
+
+	public int priorityFind(char ch) {
+		switch (ch) {
+		case '+':
+		case '-':
+			return 1;
+		case '*':
+		case '/':
+			return 2;
+		case '^':
+			return 3;
+		}
+		return -1;
+	}
+
+	public void evaluatePostfix(String exp) throws Exception {
+		int res = 0;
+		Stackkk<Integer> stackkk = new Stackkk<>();
+		while (exp.length() != 0) {
+			char ch = exp.charAt(0);
+			exp = exp.substring(1);
+
+			if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+				stackkk.push(ch - '0');
+			} else if ("+-*/".indexOf(ch) != -1) {
+				int one = stackkk.pop();
+				int two = stackkk.pop();
+				int tempRes = evalFunc(ch, one, two);
+
+				stackkk.push(tempRes);
+
+			}
+
+		}
+
+		System.out.println(stackkk.pop());
+	}
+
+	public int evalFunc(char ch, int one, int two) {
+		switch (ch) {
+		case '+':
+			return one + two;
+		case '-':
+			return two - one;
+		case '*':
+			return one * two;
+		case '/':
+			return two / one;
+		case '^':
+			return two ^ one;
+		}
+		return -1;
+	}
+
+	public void checkBalanceBrackets(String exp) {
+
+		Stackkk<Character> st = new Stackkk<>();
+
+		while (exp.length() != 0) {
+			char ch = exp.charAt(0);
+			exp = exp.substring(1);
+
+			if ("([{".indexOf(ch) != -1) {
+				
+			}
+		}
 
 	}
 
