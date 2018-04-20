b7d6c8125611a8ccb82a844591ef28f452f9f493
Jatin Verma <jatin95oct@gmail.com>
2018-04-20 11:12:02 +0530

    GTTillHeight

--- Trees/GenericTrees/Client.java
+++ Trees/GenericTrees/Client.java
@@ -3,8 +3,15 @@ package Trees.GenericTrees;
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1 3 2 2 5 0 6 0 3 1 7 0 4 0
+		GenricTreess gt = new GenricTreess();
 
+		gt.display();
+
+		System.out.println(gt.getSize());
+		System.out.println(gt.size2());
+
+		System.out.println(gt.max());
 	}
 
 }
--- Trees/GenericTrees/GenricTreess.java
+++ Trees/GenericTrees/GenricTreess.java
@@ -3,13 +3,13 @@ package Trees.GenericTrees;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class GenricTreess<T> {
+public class GenricTreess {
 
 	private class Node {
-		T data;
+		int data;
 		ArrayList<Node> children;
 
-		public Node(T data) {
+		public Node(int data) {
 			this.data = data;
 			this.children = new ArrayList<>();
 		}
@@ -20,10 +20,100 @@ public class GenricTreess<T> {
 
 	public GenricTreess() {
 		this.size = 0;
+		Scanner scn = new Scanner(System.in);
+
+		this.root = takeInput(scn, null, 0);
+	}
+
+	private Node takeInput(Scanner scn, Node parent, int i) {
+		if (parent == null) {
+			System.out.println("Enter root data");
+		} else {
+			System.out.println("Enter data for" + parent.data + "'s " + i + "th child");
+		}
+
+		int data = scn.nextInt();
+
+		Node child = new Node(data);
+
+		this.size++;
+
+		System.out.println("Enter no. of children for" + child.data);
+		int numGC = scn.nextInt();
+
+		for (int j = 0; j < numGC; j++) {
+			child.children.add(takeInput(scn, child, j));
+		}
+
+		return child;
 
 	}
 
-	private Node takeInput(Scanner scn, Node parent, int data) {
+	public int getSize() {
+		return this.size;
+	}
+
+	public boolean isEmpty() {
+		return this.getSize() == 0;
+	}
+
+	public void display() {
+		this.display(this.root);
+	}
+
+	private void display(Node node) {
+		System.out.print(node.data + "->");
+
+		for (int i = 0; i < node.children.size(); i++) {
+			System.out.print(node.children.get(i).data + ",");
+		}
+		System.out.println("END");
+
+		for (int i = 0; i < node.children.size(); i++) {
+			this.display(node.children.get(i));
+		}
+	}
+
+	public int size2() {
+		return this.size2(this.root);
+	}
+
+	private int size2(Node node) {
+		int count = 0;
+
+		count += 1;
+
+		for (Node children : node.children) {
+			count += size2(children);
+		}
+
+		return count;
+	}
+
+	public int max() {
+		return this.max(this.root);
+	}
+
+	private int max(Node node) {
+		int max = Integer.MIN_VALUE;
+
+		max = node.data;
+
+		for (Node children : node.children) {
+			int rv = max(children);
+			if (max < rv) {
+				max = rv;
+			}
+		}
+
+		return max;
+	}
+
+	public int height() {
+		return this.height(this.root);
+	}
+
+	private int height(Node node) {
 
 	}
 
